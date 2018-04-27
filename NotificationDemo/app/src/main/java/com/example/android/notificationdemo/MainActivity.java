3989df2093179fb2b7a2c1e73b84e7f3b27fa5ce
XyMengJie <xymengjie@163.com>
2018-04-27 16:27:27 +0800

    notification优化

--- NotificationDemo/app/src/main/java/com/example/android/notificationdemo/MainActivity.java
+++ NotificationDemo/app/src/main/java/com/example/android/notificationdemo/MainActivity.java
@@ -46,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
                 sendSubscribeMessage();
             }
         });
+        findViewById(R.id.btn_anther).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TestActivity.class));
+            }
+        });
     }
 
     @TargetApi(Build.VERSION_CODES.O)
