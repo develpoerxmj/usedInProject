5ceab1985de412e18b86d3a1a77c03cce8a0c0a3
XyMengJie <xymengjie@163.com>
2018-04-27 15:40:37 +0800

    notification 8.0适配

---
+++ NotificationDemo/app/src/main/java/com/example/android/notificationdemo/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.android.notificationdemo;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * android8.0通知适配----添加通知渠道channel
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelId = "chat";
+            String channelName = "聊天";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+
+            channelId = "subscribe";
+            channelName = "订阅消息";
+            importance = NotificationManager.IMPORTANCE_DEFAULT;
+            createNotificationChannel(channelId, channelName, importance);
+        }
+
+        findViewById(R.id.btn_chat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendChatMessage();
+            }
+        });
+        findViewById(R.id.btn_subscribe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSubscribeMessage();
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel notificationChannel = new NotificationChannel(channelId, channelName, importance);
+        notificationChannel.setShowBadge(true);//表名允许渠道下的通知显示角标
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(notificationChannel);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void sendChatMessage(){
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new Notification.Builder(this, "chat")
+                .setContentText("今天中午吃饭？")
+                .setContentTitle("收到一条聊天消息")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round))
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setNumber(5)
+                .build();
+        //得到渠道
+        NotificationChannel channel = manager.getNotificationChannel("chat");
+        if (channel.getImportance() == NotificationManager.IMPORTANCE_NONE){//表名用户屏蔽了该渠道
+            Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+            intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+            intent.putExtra(Settings.EXTRA_CHANNEL_ID, channel.getId());
+            startActivity(intent);
+            Toast.makeText(this, "请手动打开通知", Toast.LENGTH_SHORT).show();
+        }
+        manager.notify(1, notification);
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void sendSubscribeMessage(){
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new Notification.Builder(this, "subscribe")
+                .setContentTitle("收到一条订阅消息")
+                .setContentText("深圳地铁10号线将于近期开通")
+                .setWhen(System.currentTimeMillis())
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_round))
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setAutoCancel(true)
+                .build();
+        manager.notify(2, notification);
+    }
+}
---
+++ NotificationDemo/app/src/main/java/com/example/android/notificationdemo/TestActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.notificationdemo;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
